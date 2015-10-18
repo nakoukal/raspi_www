c49c2903c31d3601dd453261be5de4cc61960165
„Radek <radek@nakoukal.com>
2015-10-18 20:34:43 +0200

    modified:   actualTemp.php 	modified:   class/class.GPIO.php 	modified:   nbproject/private/private.xml 	../composer.json 	../composer.lock 	avg_temp_graph.php 	font/ 	home.jpg 	home.php 	pChart/ 	../vendor/

--- smarthome/actualTemp.php
+++ smarthome/actualTemp.php
@@ -1,7 +1,7 @@
 <?php
 include_once('globals.php');
-include_once('../class/class.HTTPAnswer.php');
-include_once('../class/class.MySQL.php');
+include_once('class/class.HTTPAnswer.php');
+include_once('class/class.MySQL.php');
 $HTTPAnswer = new HTTPAnswer();
 $oMySQL = new MySQL('temperature', $dblogin, $dbpwd, $dbhost, 3306);
 
@@ -18,7 +18,6 @@ function getRequiredTemp($oMySQL,$sensorName){
 
 $Sql = "SELECT temp01 oby,temp02 ven,temp03 krb,temp04 pod,temp11 aku, temp08 pokt, temp09 pokn FROM temp ORDER BY timestamp DESC LIMIT 1";
 $Res = $oMySQL->ExecuteSQL($Sql);
-//$HTTPAnswer->HTTPAnswer(HTTP_ANSWER_STATUS_200,$Res["temp01"]."|".$Res["temp02"]."|".$Res["temp03"]."|".$Res["temp04"]."|".$Res["temp11"],true);
 $retval['temp'][] = array('name'=>'Venek','act'=>$Res['ven'],'req'=>getRequiredTemp($oMySQL,'temp02'));
 $retval['temp'][] = array('name'=>'Obyvak','act'=>$Res['oby'],'req'=>getRequiredTemp($oMySQL,'temp01'));
 $retval['temp'][] = array('name'=>'Tom','act'=>$Res['pokt'],'req'=>getRequiredTemp($oMySQL,'temp08'));
@@ -26,5 +25,5 @@ $retval['temp'][] = array('name'=>'Nela','act'=>$Res['pokn'],'req'=>getRequiredT
 $retval['temp'][] = array('name'=>'Krb','act'=>$Res['krb'],'req'=>getRequiredTemp($oMySQL,'temp03'));
 $retval['temp'][] = array('name'=>'Podlaha','act'=>$Res['pod'],'req'=>getRequiredTemp($oMySQL,'temp04'));
 $retval['temp'][] = array('name'=>'Aku','act'=>$Res['aku'],'req'=>getRequiredTemp($oMySQL,'temp11'));
-//$retval['temp'] = "25";
+
 $HTTPAnswer->HTTPAnswer(HTTP_ANSWER_STATUS_200,json_encode($retval),true);
